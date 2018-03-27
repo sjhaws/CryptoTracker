88c179d3f5b0babac8e3acf94035737fdd1b451d
Steven Haws <stevenjhaws@gmail.com>
2018-03-26 18:37:01 -0600

    finished coin controller

--- app/controllers/api/coins_controller.rb
+++ app/controllers/api/coins_controller.rb
@@ -8,6 +8,15 @@ class Api::CoinsController < ApplicationController
   end
 
   def create
+    cmc_id = params[:coin].downcase
+    res = HTTParty.get("#{BASE_URL}#{cmc_id}")
+    if coin = Coin.create_by_cmc_id(res)
+      watched = WatchedCoin.find_or_creat_by(coin_id: coin_id, user_id: current_user.id)
+      watched.update(initial_price: coin.price) if watched.initial_price.nil?
+      render json :coin
+    else
+      render json: {errors: "Coin not found"}
+    end
   end
 
   def show
@@ -15,9 +24,15 @@ class Api::CoinsController < ApplicationController
   end
 
   def update
+    current_user.watched_coins.find_by(coin_id: @coin.id).destroy
   end
 
   def destroy
     @coin.destroy
   end
+
+  private
+    def set_coin
+      @coin = Coin.find(params[:id])
+    end
 end
